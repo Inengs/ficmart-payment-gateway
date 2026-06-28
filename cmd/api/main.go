ae697007eb9a7bd5bc6bf3bc446e2962cfbb0de6
Inengiye Emmanuel <inengiyeemmanuel@gmail.com>
2026-06-28 20:28:21 +0100

    chore: added health endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create a Gin router with default middleware (logger and recovery)
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		// Return JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello world",
+		})
+	})
+
+	// start server on port 8080
+	r.Run()
 }
